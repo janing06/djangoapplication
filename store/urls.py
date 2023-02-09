78c6e4469f228c6bf79f0bfc02dce40cb87e4d69
janing06 <120067518+janing06@users.noreply.github.com>
2023-02-09 14:48:40 +0800

    updateeee

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from store.views import store,cart,checkout,home
+from store.views import store,cart,checkout,home,removal
 
 urlpatterns = [
 
     path("",home,name='home'),
     path("store/",store,name='store'),
+    path("removal/",removal,name='removal'),
     path("cart/",cart,name='cart'),
     path("checkout/",checkout,name='checkout'),
 
--- store/views.py
+++ store/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from rembg import remove
+from PIL import Image
+import os
+from django.core.files.storage import FileSystemStorage
 
 def home(request):
 
@@ -11,6 +15,27 @@ def store(request):
     context = {}
 
     return render(request, 'store/store.html',context)
+
+def removal(request):
+
+    if request.method == 'POST' and request.FILES['upload']:
+        upload = request.FILES['upload']
+        fss = FileSystemStorage()
+        file = fss.save(upload.name, upload)
+
+        input_path = fss.path(file)
+        output_path = 'static/images/output.png'
+
+        input = Image.open(input_path)
+        output = remove(input)
+        output.save(output_path)
+        
+        file_url = fss.url('static/images/output.png')
+        os.remove(fss.path(file))
+        
+        return render(request, 'store/store.html', {'file_url': file_url})
+
+    return render(request,'store/store.html',{})
 
 def cart(request):
 
